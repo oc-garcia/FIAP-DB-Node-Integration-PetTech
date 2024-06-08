7e091e0bca03f1bda4639f8ffc657e00924faf59
oc-garcia <octaviogarcia1988@gmail.com>
2024-06-08 13:12:36 -0300

    chore: Update npm scripts and import paths

--- src/server.ts
+++ src/server.ts
@@ -3,4 +3,4 @@ import { env } from '@/env'
 
 app
   .listen({ host: '0.0.0.0', port: env.PORT })
-  .then(() => console.log(`Server is running on ${env.PORT}`))
+  .then(() => console.log(`Server is running on http://localhost:${env.PORT}`))
