d15bddb73609acea338b48493ba82b1a14be8ad0
oc-garcia <octaviogarcia1988@gmail.com>
2024-06-17 11:11:34 -0300

    refactor: Update typeorm configuration to include Product and Category entities

---
+++ src/entities/category.entity.ts
@@ -0,0 +1,17 @@
+import { Column, PrimaryGeneratedColumn } from 'typeorm'
+import { ICategory } from './models/category.interface'
+
+export class Category implements ICategory {
+  @PrimaryGeneratedColumn('increment', { name: 'id' })
+  id?: number
+
+  @Column({ name: 'name', type: 'varchar' })
+  name: string
+
+  @Column({
+    name: 'created_at',
+    type: 'timestamp without time zone',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  created_at: Date
+}
--- src/lib/typeorm/typeorm.ts
+++ src/lib/typeorm/typeorm.ts
@@ -1,5 +1,7 @@
 import { DataSource } from 'typeorm'
 import { env } from '@/env'
+import { Product } from '@/entities/product.entity'
+import { Category } from '@/entities/category.entity'
 
 export const appDataSource = new DataSource({
   type: 'postgres',
@@ -8,7 +10,7 @@ export const appDataSource = new DataSource({
   username: env.DATABASE_USER,
   password: env.DATABASE_PASSWORD,
   database: env.DATABASE_NAME,
-  entities: [],
+  entities: [Product, Category],
   logging: env.NODE_ENV === 'development',
 })
 
