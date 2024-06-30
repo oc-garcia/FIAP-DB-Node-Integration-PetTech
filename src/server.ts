e7a3c6d699ad4f93164e9e058c75d6f577e0a015
oc-garcia <octaviogarcia1988@gmail.com>
2024-06-30 15:58:35 -0300

    refactor: Update dependencies and add node-fetch and data-uri-to-buffer packages

--- src/server.ts
+++ src/server.ts
@@ -7,3 +7,5 @@ const PORT = env.PORT || 3000
 app
   .listen({ host: HOST, port: PORT })
   .then(() => console.log(`Server is running on http://${HOST}:${PORT}`))
+
+  //postgresql://root:kuvNGuhifmM9tBQtNQzyoG2oIz1L9Q9T@dpg-cptn7nl2ng1s73e3nph0-a/pettech
